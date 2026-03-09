c65aa419a4c5a360bfcd973c25e366544fade552
soumick727 <roysoumick727@gmail.com>
2026-03-09 12:26:45 +0530

    added the domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: true,
+    allowedHosts: [
+      "abcde-soumick.starnix.in" // Add your domain here
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
