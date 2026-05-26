eb3bff9f243a9ccf851848840f8475cdabc07112
Soumick <roysoumick727@gmail.com>
2026-05-26 23:00:06 +0530

    removed auth from public movie routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
-router.get("/checkAuth", verifyToken, checkAuth); // Check authentication status
+router.get("/checkAuth",  checkAuth); // Check authentication status
 
 export default router;
